f35412cabd2f7be3ad7343f3e813532b104ebb9f
Arpan Sen <41387097+typicalGtaTG@users.noreply.github.com>
2022-03-16 19:08:38 +0530

    Fixed Consistency Issue

--- index.php
+++ index.php
@@ -10,109 +10,129 @@ if (file_exists('LookingGlass/Config.php')) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html>
+
+<head>
     <meta charset="utf-8">
-    <title><?php echo $siteName; ?> - Looking Glass</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>AS142598 - Looking Glass</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="LookingGlass - Open source PHP looking glass">
-    <meta name="author" content="Telephone">
+    <meta name="description" content="AS142598 Arpan Sen Network - LookingGlass">
+    <meta name="author" content="Arpan Sen Network">
 
     <!-- IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
 
-    <!-- Styles -->
-    <link href="assets/css/<?php echo $theme; ?>.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <!-- Container -->
-    <div class="container">
+    <!-- Favicon -->
+    <link href="./assets-old/img/brand/favicon.png" rel="icon" type="image/png">
 
-      <!-- Header -->
-      <header class="header nohighlight" id="overview">
-        <div class="row">
-          <div class="span12">
-            <h1><a id="title" href="<?php echo $siteUrl; ?>"><?php echo $siteName; ?></a></h1>
-          </div>
-        </div>
-      </header>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 
-      <!-- Network Information -->
-      <section id="information">
-        <div class="row">
-          <div class="span12">
-            <div class="well">
-              <span id="legend">Network information</span><!-- IE/Safari dislike <legend> out of context -->
-              <p>Server Location: <b><?php echo $serverLocation; ?></b></p>
-              <div style="margin-left: 10px;">
-                <p><?php if (!empty($ipv6)) { echo 'Test IPv6: ',$ipv6; } ?></p>
-                <p>Test files: <?php
-                  foreach ($testFiles as $val) {
-                    echo "<a href=\"{$val}.test\" id=\"testfile\">{$val}</a> ";
-                  }
-                ?></p>
-              </div>
-              <p>Your IP Address: <b><a href="#tests" id="userip"><?php echo $_SERVER['REMOTE_ADDR']; ?></a></b></p>
-            </div>
-          </div>
-        </div>
-      </section>
+    <!-- Icons -->
+    <link href="./assets-old/vendor/nucleo/css/nucleo.min.css" rel="stylesheet">
+    <link href="./assets-old/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
 
-      <!-- Network Tests -->
-      <section id="tests">
-        <div class="row">
-          <div class="span12">
-            <form class="well form-inline" id="networktest" action="#results" method="post">
-              <fieldset>
-                <span id="legend">Network tests</span>
-                <div id="hosterror" class="control-group">
-                  <div class="controls">
-                    <input id="host" name="host" type="text" class="input-large" placeholder="Host or IP address">
-                  </div>
+    <!-- Argon CSS -->
+    <link type="text/css" href="./assets-old/css/argon.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <header>
+            <h1>
+                AS142598
+            </h1>
+            <div class="card card-frame">
+                <div class="card-header">
+                    <h2>Network Information</h2>
                 </div>
-                <select name="cmd" class="input-medium" style="margin-left: 5px;">
-                  <option value="host">host</option>
-                  <option value="mtr">mtr</option>
-                  <?php if (!empty($ipv6)) { echo '<option value="mtr6">mtr6</option>'; } ?>
-                  <option value="ping" selected="selected">ping</option>
-                  <?php if (!empty($ipv6)) { echo '<option value="ping6">ping6</option>'; } ?>
-                  <option value="traceroute">traceroute</option>
-                  <?php if (!empty($ipv6)) { echo '<option value="traceroute6">traceroute6</option>'; } ?>
-                </select>
-                <button type="submit" id="submit" name="submit" class="btn btn-primary" style="margin-left: 10px;">Run Test</button>
-              </fieldset>
-            </form>
-          </div>
-        </div>
-      </section>
-
-      <!-- Results -->
-      <section id="results" style="display:none">
-        <div class="row">
-          <div class="span12">
-            <div class="well">
-              <span id="legend">Results</span>
-              <pre id="response" style="display:none"></pre>
+                <div class="card-body">
+                    <p>Server Location: <b><?php echo $serverLocation; ?></b></p>
+                    <p>Test IPv6: <?php echo $ipv6; ?></p>
+                    <p>Test files: <?php
+                        foreach ($testFiles as $val) {
+                          echo "<a href=\"{$val}.test\" id=\"testfile\">{$val}</a> ";
+                        }
+                      ?></p>
+                    <p>Your IP Address: <b><a href="#tests" id="userip"><?php echo $_SERVER['REMOTE_ADDR']; ?></a></b>
+                    </p>
+                </div>
             </div>
-          </div>
+        </header>
+        <div class="card card-frame">
+            <div class="card-header">
+                <h2>Network Tests</h2>
+            </div>
+            <div class="card-body">
+                <form id="networktest" action="#results" method="post">
+                    <fieldset>
+                        <div id="hosterror" class="form-group control-group">
+                            <div class="controls">
+                                <input id="host" name="host" type="text" class="form-control"
+                                    placeholder="Host or IP address">
+                            </div>
+                        </div>
+                        <select name="cmd" class="form-control">
+                            <option value="host">host</option>
+                            <option value="mtr">mtr</option>
+                            <?php if (!empty($ipv6)) { echo '<option value="mtr6">mtr6</option>'; } ?>
+                            <option value="ping" selected="selected">ping</option>
+                            <?php if (!empty($ipv6)) { echo '<option value="ping6">ping6</option>'; } ?>
+                            <option value="traceroute">traceroute</option>
+                            <?php if (!empty($ipv6)) { echo '<option value="traceroute6">traceroute6</option>'; } ?>
+                        </select><br>
+                        <button type="submit" id="submit" name="submit" class="form-control btn btn-primary">Run
+                            Test</button>
+                    </fieldset>
+                </form>
+            </div>
         </div>
-      </section>
 
-      <!-- Footer -->
-      <footer class="footer nohighlight">
-        <p class="pull-right">
-            <a href="#">Back to top</a>
-        </p>
-        <p>Hosted By <a href="https://142598.xyz">Arpan Sen Network</a></p>
-      </footer>
+        <div id="results" class="card card-frame" style="display:none">
+            <div class="card-header">
+                <h2>
+                    Results
+                </h2>
+            </div>
+            <div class="card-body">
+                <div class="span12">
+                    <div class="well">
+                        <code>
+                            <pre id="response" style="display:none"></pre>
+                        </code>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    </div><!-- /container -->
+    <footer class="py-5" id="footer-main">
+        <div class="container">
+            <div class="row align-items-center justify-content-xl-between">
+                <div class="col">
+                    <div class="copyright text-center text-muted">
+                        Copyright © 2021-2022
+                        <a href="https://arpan.ovh" class="font-weight-bold ml-1" target="_blank">Arpan Sen</a>. All
+                        rights reserved.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-    <!-- Javascript -->
+    <!-- Core -->
+    <script src="./assets-old/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="./assets-old/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Argon JS -->
+    <script src="./assets-old/js/argon.min.js"></script>
     <script src="assets/js/jquery-1.11.2.min.js"></script>
     <script src="assets/js/LookingGlass.min.js"></script>
     <script src="assets/js/XMLHttpRequest.min.js"></script>
-  </body>
+</body>
+
 </html>
